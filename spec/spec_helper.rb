c73d1a70a2541126ec5c8b4b28cf70b1c7adfbb0
Prachi Bora <prachi.bora@microsoft.com>
2018-08-21 20:44:13 +0530

    Cobertura formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require 'simplecov'
+require 'simplecov-cobertura'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::CoberturaFormatter
+]
+
 SimpleCov.start
 
 require_relative '../string_ops'
